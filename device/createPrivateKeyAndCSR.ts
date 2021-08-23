ebc41479867738c0b7cfd6034c016c8d145aaff7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-08-23 13:30:09 +0200

    fix: fix semver check

--- device/createPrivateKeyAndCSR.ts
+++ device/createPrivateKeyAndCSR.ts
@@ -20,7 +20,7 @@ export const createPrivateKeyAndCSR = async ({
 	secTag: number
 }): Promise<Buffer> => {
 	const mfw = await getModemFirmware({ at })
-	if (semver.satisfies(mfw, '>=1.3.0')) {
+	if (!semver.satisfies(mfw, '>=1.3.0')) {
 		throw new Error(
 			`Please update your modem firwmare to at least version 1.3.0. Got ${mfw}.`,
 		)
