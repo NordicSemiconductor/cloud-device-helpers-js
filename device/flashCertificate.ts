422012f04bbde34a2aea936e9d0e1427c56136d6
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-10-14 09:55:01 +0200

    feat(flash-certificate): allow to flash secondary CA cert

--- device/flashCertificate.ts
+++ device/flashCertificate.ts
@@ -6,18 +6,35 @@ export const flashCertificate = async ({
 	secTag,
 	clientCert,
 	caCert,
+	secondaryCaCert,
 }: {
 	at: (cmd: string) => Promise<string[]>
 	secTag: number
 	clientCert: string
 	caCert: string
+	/**
+	 * Optionally provision a secondary CA certificate to the given sec tag
+	 */
+	secondaryCaCert?: {
+		secTag: number
+		caCert: string
+	}
 }): Promise<void> => {
 	// Turn off modem
 	await at('AT+CFUN=4')
 	// 0 – Root CA certificate (ASCII text)
-	await at(`AT%CMNG=0,${secTag},0,"${caCert.replace(/\n/g, '\r\n')}"`)
+	await at(`AT%CMNG=0,${secTag},0,"${convertNewlines(caCert)}"`)
 	// 1 – Client certificate (ASCII text)
-	await at(`AT%CMNG=0,${secTag},1,"${clientCert.replace(/\n/g, '\r\n')}"`)
+	await at(`AT%CMNG=0,${secTag},1,"${convertNewlines(clientCert)}"`)
+	if (secondaryCaCert !== undefined) {
+		await at(
+			`AT%CMNG=0,${secondaryCaCert.secTag},0,"${convertNewlines(
+				secondaryCaCert.caCert,
+			)}"`,
+		)
+	}
 	// Turn on modem
 	await at('AT+CFUN=1')
 }
+
+const convertNewlines = (s: string): string => s.replace(/\n/g, '\r\n')
