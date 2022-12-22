c6d7855d36fa761b21ef8ed10d3ea232c95e9174
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-12-23 00:46:02 +0100

    feat: remove colored output
    Closes #125

--- runner/log.ts
+++ runner/log.ts
@@ -1,5 +1,3 @@
-import * as chalk from 'chalk'
-
 const notEmpty = (s?: any) => s !== undefined
 
 const stringify = (a: any) =>
@@ -8,16 +6,12 @@ const stringify = (a: any) =>
 export type LogFN = (...text: unknown[]) => void
 
 const logWriter =
-	(
-		colorFn: (...text: unknown[]) => string,
-		logFN: LogFN,
-		withTimestamp = false,
-	) =>
+	(logFN: LogFN, withTimestamp = false) =>
 	(...args: unknown[]): void => {
 		logFN(
 			...[
-				withTimestamp ? chalk.grey(`[${new Date().toISOString()}]`) : undefined,
-				...args.map((arg) => colorFn(stringify(arg))),
+				withTimestamp ? `[${new Date().toISOString()}]` : undefined,
+				...args.map((arg) => stringify(arg)),
 			].filter(notEmpty),
 		)
 	}
@@ -36,33 +30,13 @@ export const log = ({
 	error: (...args: any[]) => void
 } => ({
 	warn: (...args: any[]) =>
-		logWriter(
-			chalk.yellow,
-			console.warn,
-			withTimestamp,
-		)(...[...(prefixes ?? []), ...args]),
+		logWriter(console.warn, withTimestamp)(...[...(prefixes ?? []), ...args]),
 	progress: (...args: any[]) =>
-		logWriter(
-			chalk.blue,
-			console.info,
-			withTimestamp,
-		)(...[...(prefixes ?? []), ...args]),
+		logWriter(console.info, withTimestamp)(...[...(prefixes ?? []), ...args]),
 	success: (...args: any[]) =>
-		logWriter(
-			chalk.green,
-			console.info,
-			withTimestamp,
-		)(...[...(prefixes ?? []), ...args]),
+		logWriter(console.info, withTimestamp)(...[...(prefixes ?? []), ...args]),
 	debug: (...args: any[]) =>
-		logWriter(
-			chalk.magenta,
-			console.debug,
-			withTimestamp,
-		)(...[...(prefixes ?? []), ...args]),
+		logWriter(console.debug, withTimestamp)(...[...(prefixes ?? []), ...args]),
 	error: (...args: any[]) =>
-		logWriter(
-			chalk.red,
-			console.error,
-			withTimestamp,
-		)(...[...(prefixes ?? []), ...args]),
+		logWriter(console.error, withTimestamp)(...[...(prefixes ?? []), ...args]),
 })
