d2da5b15b3bfd22c8c348d2358c00347979423fb
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-12-04 14:50:15 +0100

    fix: __filename does note exist in ESM

--- at_host/index.ts
+++ at_host/index.ts
@@ -1,16 +1,18 @@
 import { realpathSync } from 'fs'
 import * as path from 'path'
+import { fileURLToPath } from 'url'
+const self = fileURLToPath(import.meta.url)
 
 export const atHostHexfile = {
 	thingy91: path.join(
-		path.dirname(realpathSync(__filename)),
+		path.dirname(realpathSync(self)),
 		'..',
 		'..',
 		'at_host',
 		'thingy91_at_host_increased_buf.hex',
 	),
 	'9160dk': path.join(
-		path.dirname(realpathSync(__filename)),
+		path.dirname(realpathSync(self)),
 		'..',
 		'..',
 		'at_host',
